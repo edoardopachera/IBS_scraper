46630dc887cb4a382371875312e4aeaf1cb8d24f
LGuetta <lorenzoguetta@gmail.com>
2020-12-16 16:49:04 +0100

    corner case test

--- package/testing/test_module_installer.py
+++ package/testing/test_module_installer.py
@@ -22,7 +22,24 @@ class TestInput(unittest.TestCase):
         except Exception:
             self.fail("È stato impossibile importare il modulo %s" % (module))
 
+
     # invalid module name
     def test_non_existing_package(self):
         with self.assertRaises(Exception):
             mi.modules(['qwertyuiop'])
+
+
+    # corner case: existing but non installed module
+    def test_non_installed(self):
+        module = "selenium"
+        try:
+            subprocess.check_call([sys.executable,
+                                   "-m", "pip", "uninstall",
+                                   "-y", module])
+            mi.modules([module])
+        except Exception:
+            self.fail(ModuleNotFoundError)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
